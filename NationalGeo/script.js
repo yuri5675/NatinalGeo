49d6efd1e5a8b43dfc73a49b9b11a7db18aee289
yuri5675 <144127861+yuri5675@users.noreply.github.com>
2024-09-27 17:22:28 -0300

    Add files via upload

--- NationalGeo/script.js
+++ NationalGeo/script.js
@@ -22,3 +22,30 @@ function initTabList(){
     js_tabDescricao[0].classList.add('ativo')
 };
 initTabList()
+
+ 
+
+/* criando Accordion list:  é uma lista de itens empilahados verticalmente 
+    que ao ser clicado, revela ou oculta o conteudo da lista */
+function initAccordion(){
+    const jsAcordion=document.querySelectorAll('.js_accordion dt')
+
+    jsAcordion.forEach(itemdt =>{
+        itemdt.addEventListener('click',activeAcordian)
+
+
+    })
+   
+    function activeAcordian(){
+        this.classList.toggle('ativo')
+        this.nextElementSibling.classList.toggle('ativo');
+        /*nextElementSibling: retorna o conteudo html do proximo irmão(logo o que vem abaixo do elemento pai)*/
+
+}
+
+jsAcordion[0].classList.add('ativo')
+jsAcordion[0].nextElementSibling.classList.add('ativo');  
+   
+
+}
+initAccordion();
